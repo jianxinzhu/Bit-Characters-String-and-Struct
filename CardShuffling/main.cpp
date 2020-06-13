05f47f74154818ff96fe7c486ddce76944315ba3
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-06-13 00:45:42 -0400

    Create main.cpp

---
+++ CardShuffling/main.cpp
@@ -0,0 +1,12 @@
+/*    How To Program C++ 10th Edition
+      Author: Paul Deitel, Harvey Deitel
+*/
+
+#include"DeckOfCard.h"
+
+int main()
+{
+	DeckOfCard deckofcard;
+	deckofcard.shuffle();
+	deckofcard.deal();
+}
