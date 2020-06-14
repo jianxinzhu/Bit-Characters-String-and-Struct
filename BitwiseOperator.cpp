0ce0e2957fbe3fd73890da8cd4cfaac99a198059
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-06-14 00:56:18 -0400

    Create BitwiseOperator.cpp

---
+++ BitwiseOperator.cpp
@@ -0,0 +1,64 @@
+ /*    
+      How To Program C++ 10th Edition
+      Author: Paul Deitel, Harvey Deitel
+*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void displayBit(unsigned);
+
+int main()
+{
+	unsigned number = 2179876355;
+	unsigned mask = 1;
+	cout << "The result of combining the following\n";
+	displayBit(number);
+	displayBit(mask);
+	cout << "Using the bitwise AND operator & is\n";
+	displayBit(number & mask);
+
+	number = 15;
+	mask = 241;
+
+	cout << "\nThe result of combining following\n";
+	displayBit(number);
+	displayBit(mask);
+	cout << "Using the bitwise OR operator | is\n";
+	displayBit(number | mask);
+
+	number = 139;
+	mask = 199;
+
+	cout << "\nThe result of combining following\n";
+	displayBit(number);
+	displayBit(mask);
+	cout << "Using the bitwise XOR operator ^ is\n";
+	displayBit(number ^ mask);
+
+	number = 21845;
+
+	cout << "\nThe one's complement of\n";
+	displayBit(~number);
+
+}
+void displayBit(unsigned number)
+{
+	const unsigned SHIFT{ 8 * sizeof(unsigned) - 1 };
+	const unsigned MASK{ static_cast<const unsigned>(1 << SHIFT) };
+
+	cout << setw(10) << number << " = ";
+
+	for (unsigned i = 1; i <= SHIFT; ++i)
+	{
+		cout << (number & MASK ? '1' : '0');
+		number <<= 1;
+
+		if (i % 8 == 0)
+		{
+			cout << ' ';
+		}
+	}
+	cout << endl;
+}
